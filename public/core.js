630150a5c9d378be6c3a0d81dd1abfa9f91374b8
JamLk <jamboy5590@gmail.com>
2015-10-30 21:14:33 -0400

    first commit

--- public/core.js
+++ public/core.js
@@ -1,40 +1,79 @@
-var scotchTodo = angular.module('scotchTodo', []);
+var scotchTodo = angular.module('calc', []);
 
 function mainController($scope, $http, $filter) {
+	$scope.displayNumber = 0;
+	$scope.displayNumber2 = 0;
+	$scope.displayValue = "0";
+	$scope.memory = "0";
+	$scope.operatorSign = "";
+	$scope.answer = "0";
 
-	$scope.list = [
-		{taskName:'Move Down/Up', done:false},
-		{taskName:'Fix CSS line-thourgh', done:true},
-		{taskName:'Edit Tasks' , done:false},
-		{taskName:'Clear Done Tasks' , done:true},
-		{taskName:'Finsh CSS' , done:false}
-	];
-	$scope.todoText = "";
+	$scope.numberClicked = function(numberPushed) {
+		if ($scope.displayValue == "0") {
+			$scope.displayValue = numberPushed;
+		}
+		else if ($scope.answer != "0") {
+			$scope.displayValue = numberPushed;
+			$scope.answer = "0";
+		}
+		else {
+			$scope.displayValue += numberPushed;
+		}
 
-	$scope.grab = function(){
-		var task = {
-			done: false,
-			taskName: $scope.todoText,
-		};
+	}
+	$scope.opSign = function (signPushed) {
+		$scope.answer = "0";
+		$scope.displayNumber = $scope.displayValue;
+		$scope.displayNumber2= "0";
+		$scope.operatorSign = signPushed;
+		$scope.displayValue += signPushed;
+	}
+	$scope.clear =function(){
+		$scope.displayValue = "0";
+	}
 
-		$scope.list.push(task);
-		$scope.todoText = "";
-	};
+	$scope.clearCE =function() {
+		$scope.displayNumber = 0;
+		$scope.displayNumber2 = 0;
+		$scope.displayValue = "0";
+		$scope.memory = "0";
+		$scope.operatorSign = "";
+		$scope.answer = "0";
+	}
 
-	$scope.newList = function(){
-		$scope.list = [];
-		$scope.todoText = "";
-	};
+	$scope.del = function() {
+		$scope.displayValue = $scope.displayValue.slice(0,$scope.displayValue.length-1);
+	}
 
-	$scope.deleteBn = function(i){
-		$scope.list.splice(i,1);
-	};
+	$scope.save = function() {
+		$scope.displayValue += $scope.memory;
+	}
 
-	$scope.clearDone = function(){
-		$scope.list = $filter('filter')($scope.list, { done: "false" });
-	};
+	$scope.solve = function() {
+		$scope.displayNumber2 = $scope.displayValue.substring($scope.displayNumber.length+1,$scope.displayValue.length);
+		var x = $scope.displayNumber;
+		var y = $scope.displayNumber2;
+		var z = 0;
 
-	$scope.edit = function(e){
-		$scope.list.push(e);
-	};
+		switch ($scope.operatorSign) {
+			case '+':
+				z = Number(x) + Number(y);
+				break;
+			case '-':
+				z = Number(x) - Number(y);
+				break;
+			case '×':
+				z = Number(x) * Number(y);
+				break;
+			case '÷':
+				z = Number(x) / Number(y);
+				break;
+		}
+		z = ''+z;
+		$scope.displayValue = z;
+		$scope.answer = z;
+		$scope.memory =z;
+
+	}
+
 }
